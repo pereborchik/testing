a803dd43463c8e83cc27b8358ef8c62fa1472565
pereborchik <a1manax3910@mail.ru>
2022-11-05 18:16:26 +0300

    добавил комментик

--- server.js
+++ server.js
@@ -6,11 +6,12 @@ let requestsCount = 0
 const server = http.createServer( (request, response) => {
 
 	requestsCount++
-
+// увеличиваем количество запросов
 	switch (request.url) {
 		case '/test1 ':
 			response.write('TEST SNOVA: ' + requestsCount + '')
 			break;
+// при запросе на /test1, кидаем сообщеньку + выводим количество запросов
 		case '/test2 ':
 		    response.write('TEST 2: ' + requestsCount + '')
 		default:
