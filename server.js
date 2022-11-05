7b2fb4d0bad992eb0455f669567fbc48008c0c6e
pereborchik <a1manax3910@mail.ru>
2022-11-05 18:01:41 +0300

    U nas vse normal'no normal'no nerealno

--- server.js
+++ server.js
@@ -1,13 +1,27 @@
 const { write } = require('fs')
 const http = require('http')
 
+let requestsCount = 0
 
 const server = http.createServer( (request, response) => {
-	response.write('Test')
+
+	requestsCount++
+
+	switch (request.url) {
+		case '/test1 ':
+			response.write('TEST SNOVA: ' + requestsCount + '')
+			break;
+		case '/test2 ':
+		    response.write('TEST 2: ' + requestsCount + '')
+		default:
+			response.write('404 ')
+	}
+
+	response.write('Testdsd: ' + requestsCount + '')
 	response.end()
 
 })
 
 server.listen(3003)
 
-#testing server http.request, response
+// testing server logic
